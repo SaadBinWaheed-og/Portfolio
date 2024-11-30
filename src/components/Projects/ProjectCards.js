6905044c47090b2074e6fdff27d791af25775235
SaadBinWaheed-og <saadbnwhd@gmail.com>
2024-11-30 20:03:22 +0500

    updated css

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,32 +1,35 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { CgWebsite } from "react-icons/cg";
-import { BsAndroid, BsGithub } from "react-icons/bs";
 import { SiAndroid, SiIos } from "react-icons/si";
 
 function ProjectCards(props) {
   return (
-    <Card className="project-card-view">
-      {/* <Card.Img variant="top" src={props.imgPath} alt="card-img" /> */}
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
-        </Card.Text>
-        <Button variant="primary" href={props.androidLink} target="_blank">
-          <SiAndroid /> &nbsp;
-          {'Android'}
-        </Button>
-        {"\n"}
-        {"\n"}
-        <Button variant="primary" href={props.iosLink} target="_blank">
-          <SiIos /> &nbsp;
-          {'iOS'}
-        </Button>
-       
+    <Card className="project-card-view" style={{ height: "100%" }}>
+      <Card.Body
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <div>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text style={{ textAlign: "justify" }}>
+            {props.description}
+          </Card.Text>
+        </div>
+        <div style={{ marginTop: "auto", display: "flex", gap: "10px", justifyContent: "center", paddingTop: "10px" }}>
+          <Button variant="primary" href={props.androidLink} target="_blank">
+            <SiAndroid /> &nbsp; Android
+          </Button>
+          <Button variant="primary" href={props.iosLink} target="_blank">
+            <SiIos /> &nbsp; iOS
+          </Button>
+        </div>
       </Card.Body>
     </Card>
   );
 }
+
 export default ProjectCards;
